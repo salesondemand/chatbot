1d9b7b855eb138b60e16fe217ca6bd8fafc15fa2
Ansar mehmood <ytaura09@gmail.com>
2025-07-28 22:04:32 +0500

    h

--- onboarding_bot/settings.py
+++ onboarding_bot/settings.py
@@ -114,8 +114,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS SETTINGS
 CORS_ALLOWED_ORIGINS = [
     'https://zenith-chat-suite.lovable.app',
-    'https://ba072026eae8.ngrok-free.app',
-    'https://inplacesupport.netlify.app/',  # Optional, for local testing
+    'https://ba072026eae8.ngrok-free.app'
+      
 ]
 
 CORS_ALLOW_HEADERS = [
